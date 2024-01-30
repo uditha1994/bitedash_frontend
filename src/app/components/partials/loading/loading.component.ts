0ed97b2e57577e48166c81e5e8d064a18baf951b
uditha1994 <uditha@programmer.net>
2024-01-30 23:45:44 +0530

    loading complete

---
+++ src/app/components/partials/loading/loading.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { LoadingService } from '../../../services/loading.service';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.css'
+})
+export class LoadingComponent implements OnInit{
+  isLoading!: boolean;
+
+  constructor(loadingService: LoadingService){
+    loadingService.isLoading.subscribe((isLoading) => {
+      this.isLoading = isLoading;
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/interceptors/loading.interceptor.ts
@@ -0,0 +1,34 @@
+import { HttpRequest,HttpHandler,HttpEvent,HttpInterceptor,HttpInterceptorFn, HttpEventType } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { LoadingService } from '../../services/loading.service';
+
+var pendingRequests = 0;
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor{
+  constructor(private loadingService: LoadingService){}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.loadingService.showLoading();
+    pendingRequests = pendingRequests+1;
+    return next.handle(req).pipe(
+      tap({
+        next:(event) => {
+          if(event.type === HttpEventType.Response){
+            this.handleHideLoading();
+          }
+        },
+        error: (_) => {
+          this.handleHideLoading();
+        }
+      })
+    );
+  }
+
+  handleHideLoading(){
+    pendingRequests = pendingRequests-1;
+    if(pendingRequests === 0)
+    this.loadingService.hideLoading();
+  }
+}
